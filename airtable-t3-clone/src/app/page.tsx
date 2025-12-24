d14d18a2d541c8b81e86496b832faf91d00a9ef0
Shubh Bajpai <sbaj0015@student.monash.edu>
2025-12-24 15:48:24 +0530

    fix: space in title is missing

--- airtable-t3-clone/src/app/page.tsx
+++ airtable-t3-clone/src/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
           <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Welcome to your<span className="text-[hsl(280,100%,70%)]">Airtable!</span>
+            Welcome to your<span className="text-[hsl(280,100%,70%)]"> Airtable!</span>
           </h1>
             <div className="flex flex-col items-center justify-center gap-4">
             {session && <p className="text-center text-xl">Logged in as {session.user?.name}</p>}
