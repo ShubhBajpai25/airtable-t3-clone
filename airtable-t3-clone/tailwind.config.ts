e7793973017776ad6067849aee52a39591bb6d91
Shubh Bajpai <sbaj0015@student.monash.edu>
2026-01-02 11:26:32 +0530

    Removing v4 tailwindcss

--- airtable-t3-clone/tailwind.config.ts
+++ airtable-t3-clone/tailwind.config.ts
@@ -1,18 +1,12 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  // Scan ALL possible locations for Tailwind classes
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    // If you're using the ~/app alias
-    "./**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  
   darkMode: "class",
-  
   theme: {
     extend: {
       boxShadow: {
@@ -21,6 +15,5 @@ export default {
       },
     },
   },
-  
   plugins: [],
 } satisfies Config;
