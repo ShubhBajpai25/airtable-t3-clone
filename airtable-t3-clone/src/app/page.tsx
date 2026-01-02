5bf3710def36087a8091676591341a62f2d358c5
Shubh Bajpai <sbaj0015@student.monash.edu>
2026-01-02 12:12:50 +0530

    removing theme debug

--- airtable-t3-clone/src/app/page.tsx
+++ airtable-t3-clone/src/app/page.tsx
@@ -1,16 +1,12 @@
 import { AppLayout } from "~/app/_components/app-layout";
 import { BaseList } from "~/app/_components/base-list";
 import { api } from "~/trpc/server";
-import { ThemeDebug } from "./_components/theme-debug";
 
 export default async function HomePage() {
   const bases = await api.base.list();
   
   return (
     <>
-      {/* Debug panel - will appear as a fixed overlay in top-right */}
-      <ThemeDebug />
-      
       {/* Your normal page content */}
       <AppLayout bases={bases}>
         <BaseList />
