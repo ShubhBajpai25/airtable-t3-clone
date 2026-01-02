07dbe736027f164c16ec1799483490732e04174c
Shubh Bajpai <sbaj0015@student.monash.edu>
2026-01-02 16:27:07 +0530

    reverting for v3 for better solid understanding of problem

---
+++ airtable-t3-clone/tailwind.config.ts
@@ -0,0 +1,10 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: "class",
+  content: ["./src/**/*.{ts,tsx,js,jsx,mdx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
